3d2eb8d504ca5295a3e37670e79894dd893e30d4
im-kabirsumn <158508809+im-kabirsumn@users.noreply.github.com>
2024-03-04 00:30:37 +0600

    default structures and styles are deleted

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Demo from './Demo.jsx';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Demo />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <div>
+      <h1>This is a demo component going to be deleted</h1>
+    </div>
+  );
+};
+
+export default Demo;
